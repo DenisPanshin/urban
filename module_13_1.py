fd848451418a5fd104631257f975185dec1f4d3f
DenisPanshin <150244807+DenisPanshin@users.noreply.github.com>
2024-11-18 21:01:55 +0500

    module_13_1.py

--- module_13_1.py
+++ module_13_1.py
@@ -3,16 +3,16 @@ import asyncio
 
 async def start_strongman(name: str, power: int):
     print(f'Силач {name} начал соревнования.')
-    for i in range(1, power + 1):
-        await asyncio.sleep(power)
+    for i in range(1, 6):
+        await asyncio.sleep(10 - power)
         print(f'Силач {name} поднял {i} шар')
     print(f'Силач {name} закончил соревнования.')
 
 
 async def start_tournament():
-    task1 = asyncio.create_task(start_strongman('Denis', 3))
-    task2 = asyncio.create_task(start_strongman('Jane', 4))
-    task3 = asyncio.create_task(start_strongman('Mari', 5))
+    task1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task2 = asyncio.create_task(start_strongman('Denis', 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
     await task1
     await task2
     await task3
