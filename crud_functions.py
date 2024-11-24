753165bb1577c764ecfb30dc1106ca8366c55235
DenisPanshin <150244807+DenisPanshin@users.noreply.github.com>
2024-11-24 20:59:37 +0500

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -14,19 +14,43 @@ def initiate_db():
     price INTEGER
     )
     ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL, 
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL
+        )
+        ''')
+
+
+def add_user(username, email, age, balance=1000):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (username, email, age, balance))
+    connection.commit()
+
+
+def is_included(username):
+    cursor.execute("SELECT username FROM Users WHERE username = ?", (username,))
+    user = cursor.fetchone()
+    connection.commit()
+    if user:
+        return True
+    else:
+        return False
 
 
 def get_all_products():
     cursor.execute("SELECT * FROM Products")
     all_products = cursor.fetchall()
     connection.commit()
-    connection.close()
     return all_products
 
 
-# initiate_db()
 # for i in range(1, 5):
 #     cursor.execute("INSERT INTO Products (title, description, price) VALUES (?, ?, ?)",
 #                    (f"Название: Продукт {i}", f"Описание: Описание {i}", f"{i * 100}"))
 #
 
+connection.commit()
