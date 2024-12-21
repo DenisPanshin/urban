582274bd94e6a1c11749dbef256d0369e14280ed
DenisPanshin <150244807+DenisPanshin@users.noreply.github.com>
2024-12-21 20:28:44 +0500

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get("/user/{username}/{age}")
-async def getuserinfo(username: str, age: str) -> dict:
-    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+async def getuserinfo(username: str, age: int) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
 
 
 @app.get("/user/admin")
-async def getadmin() -> dict:
-    return {'message': 'Вы вошли как администратор'}
+async def getadmin() -> str:
+    return f'Вы вошли как администратор'
 
 
 @app.get("/user/{user_id}")
-async def getid(user_id) -> dict:
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+async def getid(user_id: int) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
 
 
 @app.get("/")
-async def main() -> dict:
-    return {'message': 'Главная страница'}
+async def main() -> str:
+    return f'Главная страница'
