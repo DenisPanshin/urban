9ce71d065e5ed310a353038dd8fa04651b00ca0c
DenisPanshin <150244807+DenisPanshin@users.noreply.github.com>
2024-12-23 20:47:54 +0500

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 from typing import Annotated
 
 app = FastAPI()
@@ -23,11 +23,15 @@ async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, ti
 async def update_user(user_id: Annotated[int, Path(ge=0, le=100, title='Enter id')],
                       username: Annotated[str, Path(min_length=5, max_length=20, title='Enter username')],
                       age: Annotated[int, Path(ge=18, le=120, title='Enter age')]) -> str:
-    users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f'The user {user_id} is updated'
+    if str(user_id) in users:
+        users[str(user_id)] = f'Имя: {username}, возраст: {age}'
+        return f'The user {user_id} is updated'
+    raise HTTPException(status_code=404, detail="User id не найден")
 
 
 @app.delete("/user/{user_id}")
 async def del_user(user_id: Annotated[int, Path(ge=0, le=100, title='Enter id')]) -> str:
-    users.pop(str(user_id))
-    return f'User {user_id} has been deleted'
+    if str(user_id) in users:
+        users.pop(str(user_id))
+        return f'User {user_id} has been deleted'
+    raise HTTPException(status_code=404, detail="User id не найден")
